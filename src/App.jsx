d4123f5cf0a5cb2adbc144802ea4a807850f100f
AliciaManyeka <amanyeka8@gmail.com>
2024-08-06 18:35:39 +0200

    first

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Card from "./Components/Cards/Card";
+import Navbar from "./Components/Navbar/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Card />
+    </>
+  );
+}
+
+export default App;
